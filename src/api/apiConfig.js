7be67909ca64389477d79b8456425dc701ae5b99
wessouzza <wes-souza@hotmail.com>
2022-02-05 13:49:42 -0300

    Page Home was added

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: //////// Visit "api.themoviedb.org/3/" to get a key \\\\\\\\,
+    baseUrl: 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1',
+    apiKey: '04c35731a5ee918f014970082a0088b1',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
