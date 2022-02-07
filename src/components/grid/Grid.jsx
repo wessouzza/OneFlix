7aa710606157a8eb7aa6841a8a6be924d067752a
wessouzza <wes-souza@hotmail.com>
2022-02-07 16:45:58 -0300

    Grid updated

--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import tmdbApi, { category, movieType, tvType } from '../../api/tmdbApi';
 import CardMovie from '../movie-card/CardMovie';
+import { OutLine } from '../button/Button';
 import './grid.scss';
 
 const Grid = props => {
@@ -36,12 +37,45 @@ const Grid = props => {
         getList();
     },[props.category, keyword]);
 
-  return( 
-    <div className='movie-grid'>
-        {
-            items.map((item, i) => <CardMovie category={props.category} item={item} key={i} />)
+    const loadMore = async () =>{
+        let response = null;
+        if(keyword === undefined){
+            const params = {
+                page: page + 1
+            };
+            switch(props.category){
+                case category.movie:
+                    response = await tmdbApi.getMoviesList(movieType.upcoming, {params});
+                break;
+                default:
+                    response = await tmdbApi.getTvList(tvType.popular, {params});
+            }
+        }else{
+            const params = {
+                page: page + 1,
+                query: keyword
+            }
+            response = await tmdbApi.search(props.category, {params});
         }
-    </div>
+        setItems([...items, ...response.results]);
+        setPage(page + 1);
+    }
+
+  return( 
+    <>
+        <div className='movie-grid'>
+            {
+                items.map((item, i) => <CardMovie category={props.category} item={item} key={i} />)
+            }
+        </div>
+        {
+            page < totalPage ? (
+                <div className='movie-grid__loadmore'>
+                    <OutLine className='small' onClick={loadMore}>Load more</OutLine>
+                </div>
+            ):null
+        }
+    </>
   );
 }
 
